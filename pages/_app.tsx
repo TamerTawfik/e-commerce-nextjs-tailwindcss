46bb37e430364a79366fb1f1d565a1bb73529620
Tamer Tawfik <tamertawfiklxr@gmail.com>
2023-03-12 07:19:23 +0200

    Added: Shipping Page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
+// @ts-nocheck comment
 import type { AppProps } from "next/app"
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 import { StoreProvider } from '@/utils/Store'
 import { Fonts } from '@/components/fonts'
@@ -7,15 +9,38 @@ import { Fonts } from '@/components/fonts'
 import '@/styles/globals.css'
 
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <>
       <Fonts />
       <SessionProvider session={session}>
         <StoreProvider>
-          <Component {...pageProps} />
+          {Component.auth ? (
+            <Auth>
+              <Component {...pageProps} />
+            </Auth>
+          ) : (
+              <Component {...pageProps} />
+            )}
         </StoreProvider>
       </SessionProvider>
     </>
   )
 }
+
+function Auth({ children }) {
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push('/unauthorized?message=login required');
+    },
+  });
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  return children;
+}
+
+export default App;
