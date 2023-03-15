5981e579c9c2cf7344fea43f0d4babb167cc79a8
Tamer Tawfik <tamertawfiklxr@gmail.com>
2023-03-15 02:26:23 +0200

    Added: Place Order Page

--- utils/Store.js
+++ utils/Store.js
@@ -60,6 +60,10 @@ function reducer(state, action) {
                     paymentMethod: action.payload,
                 },
             };
+        case 'CART_CLEAR_ITEMS':
+            return { ...state, cart: { ...state.cart, cartItems: [] } };
+
+
         default:
             return state;
     }
