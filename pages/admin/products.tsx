78c03be674ff97acf265dae05fdd427c6c680cc3
Tamer Tawfik <tamertawfiklxr@gmail.com>
2023-04-24 22:42:24 +0200

    Added: Create and Delete Button to Products

--- pages/admin/products.tsx
+++ pages/admin/products.tsx
@@ -2,6 +2,8 @@
 import axios from 'axios';
 import Link from 'next/link';
 import React, { useEffect, useReducer } from 'react';
+import { useRouter } from 'next/router';
+import { toast } from 'react-toastify';
 import Layout from '@/components/layout';
 import { Button } from '@/components/ui/button'
 import { getError } from '@/utils/error';
@@ -14,17 +16,49 @@ function reducer(state, action) {
             return { ...state, loading: false, products: action.payload, error: '' };
         case 'FETCH_FAIL':
             return { ...state, loading: false, error: action.payload };
+        case 'CREATE_REQUEST':
+            return { ...state, loadingCreate: true };
+        case 'CREATE_SUCCESS':
+            return { ...state, loadingCreate: false };
+        case 'CREATE_FAIL':
+            return { ...state, loadingCreate: false };
+        case 'DELETE_REQUEST':
+            return { ...state, loadingDelete: true };
+        case 'DELETE_SUCCESS':
+            return { ...state, loadingDelete: false, successDelete: true };
+        case 'DELETE_FAIL':
+            return { ...state, loadingDelete: false };
+        case 'DELETE_RESET':
+            return { ...state, loadingDelete: false, successDelete: false };
         default:
             state;
     }
 }
 
 export default function AdminProdcutsScreen() {
-    const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    const router = useRouter();
+
+    const [{ loading, error, products, loadingCreate, successDelete, loadingDelete }, dispatch] = useReducer(reducer, {
         loading: true,
         products: [],
         error: '',
     });
+
+    const createHandler = async () => {
+        if (!window.confirm('Are you sure?')) {
+            return;
+        }
+        try {
+            dispatch({ type: 'CREATE_REQUEST' });
+            const { data } = await axios.post(`/api/admin/products`);
+            dispatch({ type: 'CREATE_SUCCESS' });
+            toast.success('Product created successfully');
+            router.push(`/admin/product/${data.product._id}`);
+        } catch (err) {
+            dispatch({ type: 'CREATE_FAIL' });
+            toast.error(getError(err));
+        }
+    };
 
     useEffect(() => {
         const fetchData = async () => {
@@ -37,8 +71,29 @@ export default function AdminProdcutsScreen() {
             }
         };
 
-        fetchData();
-    }, []);
+        if (successDelete) {
+            dispatch({ type: 'DELETE_RESET' });
+        } else {
+            fetchData();
+        }
+    }, [successDelete]);
+
+    const deleteHandler = async (productId) => {
+        console.log(productId)
+        if (!window.confirm('Are you sure?')) {
+            return;
+        }
+        try {
+            dispatch({ type: 'DELETE_REQUEST' });
+            await axios.delete(`/api/admin/products/${productId}`);
+            dispatch({ type: 'DELETE_SUCCESS' });
+            toast.success('Product deleted successfully');
+        } catch (err) {
+            dispatch({ type: 'DELETE_FAIL' });
+            toast.error(getError(err));
+        }
+    };
+
 
     return (
         <Layout title="Admin Products">
@@ -62,7 +117,17 @@ export default function AdminProdcutsScreen() {
                     </ul>
                 </div>
                 <div className="overflow-x-auto md:col-span-3">
-                    <h1 className="mb-4 text-xl">Products</h1>
+                    <div className="flex justify-between">
+                        <h1 className="mb-4 text-xl">Products</h1>
+                        {loadingDelete && <div>Deleting item...</div>}
+                        <Button
+                            disabled={loadingCreate}
+                            onClick={createHandler}
+                            className="primary-button"
+                        >
+                            {loadingCreate ? 'Loading' : 'Create'}
+                        </Button>
+                    </div>
                     {loading ? (
                         <div>Loading...</div>
                     ) : error ? (
@@ -95,7 +160,10 @@ export default function AdminProdcutsScreen() {
                                                             <Button className='mr-3'> Edit</Button>
                                                         </Link>
                                                         &nbsp;
-                                                        <Button variant="destructive">Delete</Button>
+                                                        <Button
+                                                            onClick={() => deleteHandler(product._id)}
+                                                            type="button"
+                                                            variant="destructive">Delete</Button>
                                                     </td>
                                                 </tr>
                                             ))}
